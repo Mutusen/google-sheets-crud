b471f5b3ddfc03fcb66011847e702e8f9da5d4bd
Matthieu Desplantes <matmututu@gmail.com>
2021-12-14 16:24:24 +0100

    fixed bug when getting column letter from number

--- src/GoogleSheetsCRUD.php
+++ src/GoogleSheetsCRUD.php
@@ -205,7 +205,7 @@ class GoogleSheetsCRUD
 	{
 		$r = '';
 		for ($i = 1; $n >= 0 && $i < 10; $i++) {
-			$r = chr(0x41 + ($n % pow(26, $i) / pow(26, $i - 1))) . $r;
+			$r = chr(intval(0x41 + ($n % pow(26, $i) / pow(26, $i - 1)))) . $r;
 			$n -= pow(26, $i);
 		}
 		return $r;
